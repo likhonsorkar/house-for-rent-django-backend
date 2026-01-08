1db67a1b8d122e91956a009f828a1b44dc43f3d1
unknown <likhonsorkar002@gmail.com>
2026-01-08 21:16:46 +0600

    Debug False

--- houseforrent/settings.py
+++ houseforrent/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*^2qu^g6n(#qpg$xq@opeqmld2gfo(jvv9nt%3f3gjpg@sumkp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
