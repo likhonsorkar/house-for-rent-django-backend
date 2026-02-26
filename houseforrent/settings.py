eda69e53807131cb8dd0afac9bc9c50beeaf17fe
unknown <likhonsorkar002@gmail.com>
2026-02-26 12:12:42 +0600

    Email problem

--- houseforrent/settings.py
+++ houseforrent/settings.py
@@ -146,11 +146,11 @@ STATIC_FILE_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 #Email Setup
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_USE_TLS = config('EMAIL_TLS')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_USE_SSL = config('EMAIL_SSL')
+EMAIL_USE_SSL = False
 DEFAULT_FROM_EMAIL = config('EMAIL_HOST_USER') 
 
 AUTH_USER_MODEL = 'users.User'
